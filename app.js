a365cf92e6a01f46227a22ef24bb63f2d388a28c
Darnell <darnjames@gmail.com>
2024-08-18 01:14:32 -0400

    Add style rules for navbar and forms

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if (app.get("env") === "production") {
   sessionConfig.cookie.secure = true;
 }
 
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("dev"));
